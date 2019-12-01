b54f245695dd317a699cf4f73d8f1e985280516e
Niels Meyering <n.meyering@salt-and-pepper.eu>
2019-12-01 23:15:48 +0100

    Day 1 part 2

--- day01/day01.fsx
+++ day01/day01.fsx
@@ -1,11 +1,20 @@
-// Learn more about F# at http://fsharp.org
-
-open System
 open System.IO
 
 let fuelCost x =
     x / 3 - 2
 
-let lines = File.ReadAllLines(@"day01/input.txt") |> Array.toList |> List.map int
 
-List.sumBy fuelCost lines
+let recursiveCosts weight =
+    let subCosts fuel =
+        if fuel <= 0 then
+            None
+        else
+            Some (fuel, fuelCost fuel)
+    fuelCost weight |> Seq.unfold subCosts
+
+let realFuelCost = Seq.sum << recursiveCosts
+
+let weights = File.ReadAllLines(@"day01/input.txt") |> Array.toList |> List.map int
+
+let answer1 = List.sumBy fuelCost weights
+let answer2 = List.sumBy realFuelCost weights
