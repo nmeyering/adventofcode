b22c4c8c49576cb88a9f4e2ed84aaca79d50c801
Niels Meyering <nmeyering@gmail.com>
2021-12-13 22:54:45 +0100

    day 12

---
+++ 2021/day12/Part2.fsx
@@ -0,0 +1,82 @@
+﻿open System
+open System.IO
+
+type Cave = Start | End | Big of string | Small of string
+
+let parse (line: string): Cave * Cave =
+    let parse' (s: string): Cave =
+        match s with
+        | "start" -> Start
+        | "end" -> End
+        | _ ->
+            match (s |> Seq.toList) with
+            | c :: _ when Char.IsUpper c -> Big s
+            | c :: _ when Char.IsLower c -> Small s
+            | _ -> failwith "invalid cave name"
+    
+    match line.Split('-') with
+    | [| src; dest |] -> parse' src, parse' dest
+    | _ -> failwith "invalid line"
+
+let graph =
+    File.ReadLines(@"2021/day12/input.txt")
+    |> Seq.map parse
+    |> Seq.toList
+
+let out graph cave =
+    let forwardEdges = graph |> List.filter (fst >> (=)cave)
+    let backEdges =
+        graph
+        |> List.choose (fun edge -> 
+            match edge with
+            | (Big b, a) when a = cave -> Some (cave, Big b)
+            | _ -> None
+        )
+    List.append forwardEdges backEdges
+
+let isSmall = function Small _ -> true | _ -> false
+let smallCaveAllowed path c =
+    let counts =
+        path
+        |> List.filter isSmall
+        |> List.countBy id
+    match counts |> List.tryFind (fun (x, _) -> x = c) with
+    | None -> true
+    | Some (_, 1) -> 
+        counts
+        |> List.filter (snd >> (fun x -> x > 1))
+        |> List.isEmpty
+    | _ -> false
+
+let allowedEdges2 (path: Cave list) (edges: (Cave * Cave) list): (Cave * Cave) list =
+    let allowed (h :: rest as path) (a, b as edge) =
+        match edge with
+        | x, End
+        | x, Big _ when x = h -> Some (a, b)
+        | x, Small _ when x = h && smallCaveAllowed path b -> Some (a, b)
+        | End, x
+        | Big _, x when x = h -> Some (b, a)
+        | Small _, x when x = h && smallCaveAllowed path a -> Some (b, a)
+        | _ -> None
+
+    match path with
+    | [] -> failwith "empty path"
+    | _ -> edges |> List.choose (allowed path)
+
+let extend2 (graph: (Cave * Cave) list) (path: Cave list): Cave list list =
+    match path with
+    | [] -> failwith "empty path"
+    | End :: _ -> [path]
+    | _ ->
+    allowedEdges2 path graph
+    |> List.map snd
+    |> List.map (fun dst -> dst :: path)
+
+let allPaths2 graph =
+    let rec allPaths' paths =
+        let unfinished = paths |> List.filter (fun (h :: path) -> h <> End) |> List.length
+        if unfinished = 0 then paths else
+        paths |> List.collect (extend2 graph) |> allPaths'
+    allPaths' [[Start]]
+
+allPaths2 graph |> List.length |> printfn "part 2: %A"
---
+++ 2021/day12/Program.fsx
@@ -0,0 +1,63 @@
+﻿open System
+open System.IO
+
+type Cave = Start | End | Big of string | Small of string
+
+let parse (line: string): Cave * Cave =
+    let parse' (s: string): Cave =
+        match s with
+        | "start" -> Start
+        | "end" -> End
+        | _ ->
+            match (s |> Seq.toList) with
+            | c :: _ when Char.IsUpper c -> Big s
+            | c :: _ when Char.IsLower c -> Small s
+            | _ -> failwith "invalid cave name"
+    
+    match line.Split('-') with
+    | [| src; dest |] -> parse' src, parse' dest
+    | _ -> failwith "invalid line"
+
+let graph =
+    File.ReadLines(@"2021/day12/input.txt")
+    |> Seq.map parse
+    |> Seq.toList
+
+let allowedEdges (path: Cave list) (edges: (Cave * Cave) list): (Cave * Cave) list =
+    let smallCaveAllowed c path =
+        path
+        |> List.filter ((=)c)
+        |> List.length
+        |> (fun x -> x < 1)
+
+    let allowed (h :: rest as path) (a, b as edge) =
+        match edge with
+        | x, End
+        | x, Big _ when x = h -> Some (a, b)
+        | x, Small _ when x = h && smallCaveAllowed b path -> Some (a, b)
+        | End, x
+        | Big _, x when x = h -> Some (b, a)
+        | Small _, x when x = h && smallCaveAllowed a path -> Some (b, a)
+        | _ -> None
+
+    match path with
+    | [] -> failwith "empty path"
+    | _ -> edges |> List.choose (allowed path)
+
+let extend (graph: (Cave * Cave) list) (path: Cave list): Cave list list =
+    match path with
+    | [] -> failwith "empty path"
+    | End :: _ -> [path]
+    | _ ->
+    allowedEdges path graph
+    |> List.map snd
+    |> List.map (fun dst -> dst :: path)
+
+let allPaths graph =
+    let rec allPaths' paths =
+        let unfinished = paths |> List.filter (fun (h :: path) -> h <> End) |> List.length
+        if unfinished = 0 then paths else
+        paths |> List.collect (extend graph) |> allPaths'
+    allPaths' [[Start]]
+
+allPaths graph |> List.length |> printfn "part 1: %A"
